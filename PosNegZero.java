e56e3d5c98c6e2b9c6cf13ca21d21e36f5a97979
Pradeep <30965248+PRADEEPMALAISAMY@users.noreply.github.com>
2018-07-15 19:54:07 +0530

    Add files via upload

---
+++ PosNegZero.java
@@ -0,0 +1,18 @@
+package digipro;
+
+public class PosNegZero {
+public static void main(String[] args) {
+int a=9;
+
+if(a>0) {
+	System.out.println("Positive");
+}
+else if(a<0) {
+	System.out.println("Negative");
+}
+else {
+	System.out.println("Zero");
+}
+	
+}
+}
